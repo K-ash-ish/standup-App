939c1c7e5e9149c001d60eddcd46b70651d1cd2c
K-ash-ish <kashishsondhiya@gmail.com>
2022-10-27 00:14:42 +0530

    card design completed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,30 @@
 import React from "react";
-
-function Card(props){
-    return <h1>Card</h1>
+import "./Card.css";
+function Card(props) {
+  const date = new Date();
+  const currentDate =
+    date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear();
+  console.log(currentDate);
+  return (
+    <div className="card-container">
+      <h3>{currentDate}</h3>
+      <div className="standup">
+        <dl className="standup-list">
+          <dt>Yesterday:</dt>
+          <dd className="list-item">Hello world world world</dd>
+          <dd className="list-item">Hello world world world</dd>
+          <dd className="list-item">Hello world world world</dd>
+        </dl>
+      </div>
+      <div className="standup">
+        <dl className="standup-list">
+          <dt>Today:</dt>
+          <dd className="list-item">Hello world world world</dd>
+          <dd className="list-item">Hello world world world</dd>
+          <dd className="list-item">Hello world world world</dd>
+        </dl>
+      </div>
+    </div>
+  );
 }
 export default Card;
