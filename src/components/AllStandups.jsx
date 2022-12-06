2eaaa84af33ceaf7acc9bcdb50dd2fc3b8602246
K-ash-ish <kashishsondhiya@gmail.com>
2022-12-06 12:56:22 +0530

    progress bar for All standup page

--- src/components/AllStandups.jsx
+++ src/components/AllStandups.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Card from "./Card";
+import "./AllStandups.css"
 import { v4 as uuidv4 } from "uuid";
 function AllStandups(props) {
   const { allStandups } = props;
   return (
     <section className="all-standups">
       <h2>All Standups</h2>
+      <div className="progress-bar">
+        <div className="red-box">Active Task: </div>
+        <div className="green-box">Completed Task: </div>
+      </div>
       <div className="standups-grid">
         {allStandups.map((standup) => {
           return (
