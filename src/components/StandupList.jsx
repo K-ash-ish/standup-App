7847d5281dda5e34185389648a4c5f142e027397
K-ash-ish <kashishsondhiya@gmail.com>
2022-12-06 12:04:20 +0530

    delete button added working fine

--- src/components/StandupList.jsx
+++ src/components/StandupList.jsx
@@ -1,36 +1,45 @@
 import React from "react";
 import "./Card.css";
 import { v4 as uuidv4 } from "uuid";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 function StandupList(props) {
   const { name, standups, handleClick } = props;
+  function deleteListItem(event) {
+    let target = event.target.parentNode.parentNode.parentNode;
+    handleClick(target);
+  }
   return (
     <div className="standup">
       <dl className="standup-list">
         <dt className="list-heading">{name}:</dt>
         {standups.map((data) => {
           return name === "Yesterday" ? (
-            <dd
-              onClick={(event) => {
-                handleClick(event.target);
-              }}
-              key={uuidv4()}
-              id={data.id}
-              className="list-item"
-            >
-              {data.standup}
+            <dd key={uuidv4()} id={data.id} className="list-item">
+              <div className="item">
+                {data.standup}
+                <FontAwesomeIcon
+                  className="delete"
+                  onClick={(event) => {
+                    deleteListItem(event);
+                  }}
+                  icon={faTrash}
+                />
+              </div>
             </dd>
-            
           ) : (
-            <dd
-              onClick={(event) => {
-                handleClick(event.target);
-              }}
-              key={uuidv4()}
-              id = {data.id}
-              className="list-item"
-            >
-              {data.standup}
+            <dd key={uuidv4()} id={data.id} className="list-item">
+              <div className="item">
+                {data.standup}
+                <FontAwesomeIcon
+                  className="delete"
+                  onClick={(event) => {
+                    deleteListItem(event);
+                  }}
+                  icon={faTrash}
+                />
+              </div>
             </dd>
           );
         })}
